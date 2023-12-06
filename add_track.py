2fbbc19eb41aea64048d29882146027c04c845f8
peterwu <peterwu@qq.com>
2023-12-06 17:42:00 +0800

    修改pixel代码

--- add_track.py
+++ add_track.py
@@ -19,7 +19,7 @@ new_str = """!function(f,b,e,v,n,t,s)
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '316067037886671');
+  fbq('init', '1057702485679146');
   fbq('track', 'PageView');"""
 new_tag1.string = new_str
 bs.head.append(new_tag1)
