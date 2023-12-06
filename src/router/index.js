df5e7bcd02c7b14a20740ef2b125e23a4c2cc447
peterwu <peterwu@qq.com>
2023-12-06 17:24:10 +0800

    新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,11 @@ var router = new Router({
       component: resolve => (require(["@/components/mobile/GfIndex"], resolve))
     },
     {
+      path: '/apps/details',
+      name: 'detail',
+      component: resolve => (require(["@/components/mobile/GfIndex"], resolve))
+    },
+    {
       path: '/contact',
       name: 'contact',
       component: resolve => (require(["@/components/mobile/Contact"], resolve))
