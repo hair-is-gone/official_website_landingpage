6d19704e30533e58158db2b0ac6b2c99003f8086
peterwu <peterwu@qq.com>
2023-12-06 17:26:55 +0800

    修改chn

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -59,7 +59,7 @@ async function loadPage(){
 
 function chnup(event) {
     // return
-    let chn = "com.ludo.fire.lfha"
+    let chn = "com.ludo.up.luha"
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", ServerUrl + "/api/sys/chnup/", true);
     if(event == "load"){
