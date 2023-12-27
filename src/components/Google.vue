98d7dfebb25849f72eed5e36ba8effc3bb51be64
peterwu <peterwu@qq.com>
2023-12-27 16:39:09 +0800

    修改样式

--- src/components/Google.vue
+++ src/components/Google.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
+    <div class="dialog-box" id="gpdialog-box" @click.stop="gpcloseDialog()">
+      <div class="mid-box click-show-box">
+        <img src="../../static/mobile/left.png" @click.stop="gpchangePict(-1)" id="gpleft-pict" alt="" class="left-pict">
+        <img src="../../static/mobileb/image_1.png" alt="" class="show-pict-big" id="gpshow-pict-big">
+        <img src="../../static/mobile/right.png" class="right-pict" @click.stop="gpchangePict(1)" id="gpright-pict" alt="">
+      </div>
+    </div>
     <div class="main">
       <div class="google-head">
         <div class="google-play-pict">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/googleplay.png" alt="" class="pict">
+          <img src="../../static/mobile/googleplay.png" alt="" class="pict">
         </div>
         <div class="search-box">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_search.png" alt="" class="pict">
+          <img src="../../static/mobile/ic_search.png" alt="" class="pict">
         </div>
         <div class="question-box">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_question.png" alt="" class="pict">
+          <img src="../../static/mobile/ic_question.png" alt="" class="pict">
         </div>
       </div>
       <div class="flex-start-box icon-box">
         <div class="icon-app">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobileb/icon.png" alt="" class="pict">
+          <img src="../../static/mobileb/icon.png" alt="" class="pict">
         </div>
         <div class="app-info">
-          <span class="app-name">Ludo Up</span>
-          <span class="app-team">Up Studio</span>
+          <span class="app-name">Fruit League</span>
+          <span class="app-team">Fruit Mania</span>
           <span class="app-ads">In-App Purchases</span>
         </div>
       </div>
@@ -27,22 +34,22 @@
           <div class="app-review">
             <div class="star-box">
               <span class="mark-num">4.9</span>
-              <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_star.png" alt="" class="star-single">
+              <img src="../../static/mobile/ic_star.png" alt="" class="star-single">
             </div>
-            <span class="mark-data-grey">4k reviews
-              <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_gth.png" alt="" class="gth-pict gth-pict-1">
+            <span class="mark-data-grey">8k reviews
+              <img src="../../static/mobile/ic_gth.png" alt="" class="gth-pict gth-pict-1">
             </span>
           </div>
           <div class="split-col"></div>
           <div class="app-download">
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_xz.png" alt="" class="download-icon">
+            <img src="../../static/mobile/ic_xz.png" alt="" class="download-icon">
             <span class="mark-data-grey mark-data-grey-1">3 MB</span>
           </div>
           <div class="split-col"></div>
           <div class="app-download">
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_16.png" alt="" class="year-icon">
+            <img src="../../static/mobile/ic_16.png" alt="" class="year-icon">
             <span class="mark-data-grey">Rated for 16+
-              <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_gth.png" alt="" class="gth-pict gth-pict-2">
+              <img src="../../static/mobile/ic_gth.png" alt="" class="gth-pict gth-pict-2">
             </span>
           </div>
           <div class="split-col"></div>
@@ -58,43 +65,43 @@
 
       <div class="wish-share-box">
         <div class="wish-share-1">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_fx.png" alt="" class="share-link-pict">
+          <img src="../../static/mobile/ic_fx.png" alt="" class="share-link-pict">
           <span class="wish-share-text">Share</span>
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_qd.png" alt="" class="share-add-pict">
+          <img src="../../static/mobile/ic_qd.png" alt="" class="share-add-pict">
           <span class="wish-share-text">Add to wishlist</span>
         </div>
         <div class="wish-share-1 wish-share-2">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_equipment.png" alt="" class="share-eq-pict">
+          <img src="../../static/mobile/ic_equipment.png" alt="" class="share-eq-pict">
           <span class="wish-share-text-grey">This app works on all your devices</span>
         </div>
       </div>
 
       <div class="flex-start-box app-show-box">
         <div class="show-pict-box">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobileb/img_1.png" alt="" class="show-pict">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobileb/img_2.png" alt="" class="show-pict">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobileb/img_3.png" alt="" class="show-pict">
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobileb/img_4.png" alt="" class="show-pict">
+          <img src="../../static/mobileb/img_1.png" alt="" class="show-pict" @click="gpshowDialog(1)">
+          <img src="../../static/mobileb/img_2.png" alt="" class="show-pict" @click="gpshowDialog(2)">
+          <img src="../../static/mobileb/img_3.png" alt="" class="show-pict" @click="gpshowDialog(3)">
+          <img src="../../static/mobileb/img_4.png" alt="" class="show-pict" @click="gpshowDialog(4)">
         </div>
       </div>
 
       <div class="flex-start-box about-game-box">
         <div class="about-title-arrow-box">
           <span class="about-title">About this game</span>
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_left.png" alt="" class="right-arrow-pict right-arrow-pict-margin">
+          <img src="../../static/mobile/ic_left.png" alt="" class="right-arrow-pict right-arrow-pict-margin">
         </div>
         <span class="about-content">
-          Ludo Up combines the classic charm of Ludo with the exhilaration of real gold rewards. Roll the dice, strategize your moves, and engage in a gaming experience where triumphs translate into tangible riches. Turn
-          every move into a step towards real cash. In Ludo Up, victory is not just a title; it's a gateway to a world of cash-fueled triumphs. Play strategically, claim your wins, and unlock the path to prosperity.
+          Embark on a juicy adventure with Fruit League, the game that blends the thrill of spinning fruits with the excitement of winning real gold rewards! It's your ticket to a fruity fortune. Spin and savor the
+          sweetness of victory as you immerse yourself in the world of Fruit League. Indulge in the spinning sensation and watch as the fruits align to unlock real cash rewards! Fruit League adds a dash of excitement to
+          your gaming experience.
           <br>
           <br>
-          Features in Ludo Up: <br>
-          1. Abundant real cash rewards <br>
-          2. Strategic moves & golden triumphs <br>
-          3. Transform your victories into tangible treasures <br>
-          4. Secure and fair play  <br>
+          Features: <br>
+          1. Real cash & real triumphs <br>
+          2. Hassle-free withdrawals guaranteed <br>
+          3. Vibrant graphics and engaging gameplay <br>
+          4. Experience the joy of victory and the satisfaction of winning real cash rewards <br>
         </span>
-        <!-- <span class="collapse-span">Collapse</span> -->
       </div>
 
       <div class="flex-change-box share-box">
@@ -115,7 +122,7 @@
       <div class="flex-start-box rating-box">
         <div class="about-title-arrow-box">
           <span class="rating-title">Ratings and reviews</span>
-          <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_left.png" alt="" class="right-arrow-pict">
+          <img src="../../static/mobile/ic_left.png" alt="" class="right-arrow-pict">
         </div>
         <span class="rating-mid-content">
           Ratings and reviews are verified and are from people who use the same type of device that you use
@@ -123,11 +130,11 @@
         <div class="rate-card-box">
           <div class="rate-left">
             <span class="rate-num">4.9</span>
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_pinfen.png" alt="" class="five-star-pict five-margin-rate">
-            <span class="rate-count">4,376</span>
+            <img src="../../static/mobile/ic_pinfen.png" alt="" class="five-star-pict five-margin-rate">
+            <span class="rate-count">8,452</span>
           </div>
           <div class="rate-right">
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_dengji.png" alt="" class="pict">
+            <img src="../../static/mobile/ic_dengji.png" alt="" class="pict">
           </div>
         </div>
       </div>
@@ -135,12 +142,12 @@
       <div class="user-box">
         <div class="user-card" v-for="user in userInfoS" :key="user.name">
           <div class="flex-start-box head-avatar-width">
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_people.png" alt="" class="avatar">
+            <img src="../../static/mobile/ic_people.png" alt="" class="avatar">
             <span class="user-name">{{ user.name }}</span>
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_more.png" alt="" class="more-pict">
+            <img src="../../static/mobile/ic_more.png" alt="" class="more-pict">
           </div>
           <div class="flex-start-box user-mark-box">
-            <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_pinfen.png" alt="" class="five-star-pict">
+            <img src="../../static/mobile/ic_pinfen.png" alt="" class="five-star-pict">
             <span class="user-date">{{ user.date }}</span>
           </div>
           <span class="flex-start-box user-content">
@@ -182,7 +189,7 @@
       </div>
 
       <div class="flag-box">
-        <img src="https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_lndia.png" alt="" class="india-pict">
+        <img src="../../static/mobile/ic_lndia.png" alt="" class="india-pict">
         <span class="flag-text">India(Hindi)</span>
       </div>
 
@@ -200,52 +207,52 @@
 </template>
 
 <script>
-import { download, chnup } from "../utils/tools";
+import { download, chnup, loadPage } from "../utils/tools";
 
 export default {
   data() {
     return {
       userInfoS: [
         {
-          name: "Sharya Thakur",
-          date: "2023/11/27",
+          name: "Gopal Nayar",
+          date: "12/24/23",
           content:
-          "Ludo Up has transformed my gaming experience completely. The integration of real cash rewards adds a thrilling dimension to every move. The strategic gameplay keeps me engaged, and the fact that my victories are translated into tangible cash is absolutely fantastic. Highly recommend to those who seek the excitement of winning real cash! ",
+            "I am absolutely hooked on Fruit League. The combination of fruity excitement and the chance to win real gold is unbeatable. The graphics are vibrant, the gameplay is smooth. The withdrawal process is straightforward and quick. Highly recommend for those who love the thrill of winning real cash.",
         },
         {
-          name: "Megh Seth",
-          date: "2023/11/22",
+          name: "Anand Heravdakar",
+          date: "12/15/23",
           content:
-          "The arena, where I can compete with opponents who are extremely intelligent for real gold rewards, is truly exhilarating. "
+            "The variety of fruit-themed slots keeps things interesting, and the potential for cash rewards adds an extra layer of thrill. The game is easy to navigate, and the secure withdrawal process makes the entire gaming experience trustworthy. If you're looking for a game that blends fun with the opportunity to win real money, Fruit League is a must-try! ",
         },
         {
-          name: "Prakash Vaikar",
-          date: "2023/11/15",
+          name: "Bhrigu Srivastav",
+          date: "12/10/23",
           content:
-          "Ludo Up provides an immersive gaming experience. Knowing that my victories are rightfully rewarded with real gold adds an extra layer of excitement. It's the perfect blend of secure gameplay and the allure of golden triumphs! "
+            "The colorful fruit graphics are a feast for the eyes, and the chance to turn your gaming skills into actual cash prizes keeps me coming back for more. What I love is the opportunity to cash out real rewards.",
         },
       ],
       shareInfoS: [
         {
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_lianjie.png",
+          pict: "../../static/mobile/ic_lianjie.png",
           pictStyle: "share-pict-1",
           content: "This app may share these data types with third parties",
           content2: "App activity and Device or other IDs",
         },
         {
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_yun.png",
+          pict: "../../static/mobile/ic_yun.png",
           pictStyle: "share-pict-2",
           content: "This app may collect these data types",
           content2:
             "App activity,App info and perfor mance.and Device or other IDs",
         },
         {
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_lock.png",
+          pict: "../../static/mobile/ic_lock.png",
           pictStyle: "share-pict-3",
           content: "Data is encrypted in transit",
         },
         {
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_ljt.png",
+          pict: "../../static/mobile/ic_ljt.png",
           pictStyle: "share-pict-4",
           content: "You can request that data be deleted",
         },
@@ -253,31 +260,31 @@ export default {
       bottomInfoS: [
         {
           index: 1,
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_Games.png",
+          pict: "../../static/mobile/ic_Games.png",
           pictStyle: "bottom-pict-1",
           title: "Games",
         },
         {
           index: 2,
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_Apps.png",
+          pict: "../../static/mobile/ic_Apps.png",
           pictStyle: "bottom-pict-2",
           title: "Apps",
         },
         {
           index: 3,
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_Movies.png",
+          pict: "../../static/mobile/ic_Movies.png",
           pictStyle: "bottom-pict-3",
           title: "Movies & TV",
         },
         {
           index: 4,
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_Books.png",
+          pict: "../../static/mobile/ic_Books.png",
           pictStyle: "bottom-pict-4",
           title: "Books",
         },
         {
           index: 5,
-          pict: "https://dapv7y4era0s5.cloudfront.net/ludoup/mobile/ic_Children.png",
+          pict: "../../static/mobile/ic_Children.png",
           pictStyle: "bottom-pict-5",
           title: "Children",
         },
@@ -291,6 +298,50 @@ export default {
     },
     head_click: function () {
       chnup("headClick1");
+    },
+    gpchangePict: function (msg) {
+      let changeIndex = this.nowPict + msg;
+      if (changeIndex <= 0) {
+        let leftDiv = document.getElementById("gpleft-pict");
+        leftDiv.style.visibility = "hidden";
+        return;
+      } else if (changeIndex > 4) {
+        let rightDiv = document.getElementById("gpright-pict");
+        rightDiv.style.visibility = "hidden";
+        return;
+      } else {
+        this.nowPict = changeIndex;
+        let leftDiv = document.getElementById("gpleft-pict");
+        leftDiv.style.visibility = "visible";
+        let rightDiv = document.getElementById("gpright-pict");
+        rightDiv.style.visibility = "visible";
+        let imgDiv = document.getElementById("gpshow-pict-big");
+        imgDiv.src = `../../static/mobileb/image_${changeIndex}.png`;
+      }
+    },
+    gpshowDialog: function (id) {
+      console.log(id);
+      let leftDiv = document.getElementById("gpleft-pict");
+      leftDiv.style.visibility = "visible";
+      let rightDiv = document.getElementById("gpright-pict");
+      rightDiv.style.visibility = "visible";
+      let dialogDiv = document.getElementById("gpdialog-box");
+      dialogDiv.style.display = "block";
+      this.nowPict = id;
+      if (this.nowPict == 1) {
+        let leftDiv = document.getElementById("gpleft-pict");
+        leftDiv.style.visibility = "hidden";
+      }
+      if (this.nowPict == 4) {
+        let rightDiv = document.getElementById("gpright-pict");
+        rightDiv.style.visibility = "hidden";
+      }
+      let imgDiv = document.getElementById("gpshow-pict-big");
+      imgDiv.src = `../../static/mobileb/image_${id}.png`;
+    },
+    gpcloseDialog: function () {
+      let dialogDiv = document.getElementById("gpdialog-box");
+      dialogDiv.style.display = "none";
     },
   },
   created() {},
@@ -862,5 +913,30 @@ export default {
 }
 .bottom-card:nth-child(2) span {
   color: #0b57cf;
+}
+.dialog-box {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  display: none;
+}
+.click-show-box {
+  width: 100vw;
+  margin-top: 40vw;
+}
+.show-pict-big {
+  width: 50.94vw;
+  height: auto;
+  margin-left: 2.53vw;
+  margin-right: 2.53vw;
+}
+.left-pict {
+  width: 10vw;
+  height: 10vw;
+}
+.right-pict {
+  width: 10vw;
+  height: 10vw;
 }
 </style>
