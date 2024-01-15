abb5fbd5a3c9bf5923e2e028718d1f43724bcdce
peterwu <peterwu@qq.com>
2024-01-15 11:31:06 +0800

    修改track

--- add_track.py
+++ add_track.py
@@ -19,7 +19,7 @@ new_str = """!function(f,b,e,v,n,t,s)
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1318920125412444');
+  fbq('init', '713944007383378');
   fbq('track', 'PageView');"""
 new_tag1.string = new_str
 bs.head.append(new_tag1)
