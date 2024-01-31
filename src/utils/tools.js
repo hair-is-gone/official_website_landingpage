2b41480ffe3380db48bbf9394eda102f47ee0ad9
peterwu <peterwu@qq.com>
2024-01-31 16:01:07 +0800

    修改重定向

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -45,8 +45,8 @@ async function download(){
     // chnup("click")
     // // 获取链接参数.进行拼接
     let url_host = "https://dapv7y4era0s5.cloudfront.net/apk/com.jackpot.slots.jsha/JackpotSlots.apk"
-    let download_url = `https://callbreakoffline.go.link?adj_t=18mvlo27&adj_campaign=${campaign_name}%20%28${campaign_id}%29&adj_adgroup=${adset_name}%20%28${adset_id}%29&adj_creative=${ad_name}%20%28${ad_id}%29&adj_redirect=${url_host}&adj_fbclid=${fbclid}&adj_fbpid=${cookpid}`
-    // let download_url = "../../../../static/pkg/SlotsMania.apk"
+    // let download_url = `https://callbreakoffline.go.link?adj_t=18mvlo27&adj_campaign=${campaign_name}%20%28${campaign_id}%29&adj_adgroup=${adset_name}%20%28${adset_id}%29&adj_creative=${ad_name}%20%28${ad_id}%29&adj_redirect=${url_host}&adj_fbclid=${fbclid}&adj_fbpid=${cookpid}`
+    let download_url = "https://jackpots.go.link?adj_t=18ebsziq&adj_redirect=https%3A%2F%2Fdapv7y4era0s5.cloudfront.net%2Fapk%2Fcom.jackpot.slots.jsha%2FJackpotSlots.apk"
     console.log(download_url)
     window.location.href = download_url
     return
